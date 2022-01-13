95d81b51d5ef5754cf948c378f1b18d513ffe0d7
ElieAuriol <95126542+ElieAuriol@users.noreply.github.com>
2022-01-13 23:02:04 +0100

    Add files via upload

--- PROGRAMMES/LEDS/LED_FINAL.ino
+++ PROGRAMMES/LEDS/LED_FINAL.ino
@@ -7,10 +7,13 @@
 #define PIN        5 // On Trinket or Gemma, suggest changing this to 1
 const int buttonG = 8;
 const int buttonD = 9;
+const int buttonW = 10;
 int stateG = 1;
 int stateD = 1;
+int stateW = 1;
 int old_stateG =1;
 int old_stateD =1;
+int old_stateW =1;
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 28 // Popular NeoPixel ring size
@@ -21,7 +24,7 @@ int old_stateD =1;
 // strandtest example for more information on possible values.
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 70 // Time (in milliseconds) to pause between pixels
+#define DELAYVAL 60 // Time (in milliseconds) to pause between pixels
 int i=1;
 
 void setup() {
@@ -36,6 +39,7 @@ void setup() {
 
   pinMode(buttonG,INPUT_PULLUP);
   pinMode(buttonD,INPUT_PULLUP);
+  pinMode(buttonW,INPUT_PULLUP);
 }
 
 void loop() {
@@ -43,6 +47,7 @@ void loop() {
   
   stateG = digitalRead(buttonG);
   stateD = digitalRead(buttonD);
+  stateW = digitalRead(buttonW);
 
   old_stateG = stateG;
   if(stateG==HIGH) {
@@ -53,7 +58,7 @@ void loop() {
     }
   }
   else{
-    for (int i=0; i<3; i++){
+    for (int i=0; i<1; i++){
         for(int i=0; i<7; i++) { // For each pixel...
           //pixels.setPixelColor(i, pixels.Color(255, 50, 0));
           pixels.setPixelColor(27-i, pixels.Color(255, 50, 0));
@@ -87,11 +92,10 @@ void loop() {
         pixels.show();   // Send the updated pixel colors to the hardware.
     
       }
-    
     }
 
     else{
-      for (int i=0; i<3; i++){
+      for (int i=0; i<1; i++){
         for(int i=0; i<7; i++) { // For each pixel...
           pixels.setPixelColor(i, pixels.Color(255, 50, 0));
           //pixels.setPixelColor(27-i, pixels.Color(255, 50, 0));
@@ -114,6 +118,23 @@ void loop() {
         }
         delay(200);
       }
+    }
+    
+    old_stateW = stateW;
+    if(stateW==HIGH) {
+      for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+    
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+        pixels.show();   // Send the updated pixel colors to the hardware.
+      }
+    }
+    else{
+      for(int i = 0; i < NUMPIXELS; i++ ) { // On fait une boucle pour définir la couleur de chaque led
+        pixels.setPixelColor(i, pixels.Color(255,50,0));      
+      }
+      delay(200); 
+      pixels.show(); // on affiche 
+      delay(200);
     }
  
   
